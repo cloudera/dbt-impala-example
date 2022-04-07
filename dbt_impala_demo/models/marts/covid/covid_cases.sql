727bb5016afcf2aa571dc2dac521d650a213547e
sdairs <me@alasdairb.com>
2022-04-07 17:55:14 +0100

    fix broken mart model

--- dbt_impala_demo/models/marts/covid/covid_cases.sql
+++ dbt_impala_demo/models/marts/covid/covid_cases.sql
@@ -6,7 +6,7 @@
 
 with cases as(
   select 
-    date_rep as report_date, 
+    date_rep, 
     cases, 
     deaths, 
     geo_id
@@ -19,15 +19,15 @@ country_codes as(
   from {{ ref('ref__country_codes') }}
 )
 select 
-    cases.report_date, 
-    cases.cases, 
-    cases.deaths,
-    country_codes.country
-    from cases join country_codes on cases.geo_id = country_codes.alpha_2code;
+    cases.date_rep as report_date, 
+    cases.cases as cases, 
+    cases.deaths as deaths,
+    country_codes.country as country
+    from cases join country_codes on cases.geo_id = country_codes.alpha_2code
 
 {% if is_incremental() %}
 
   -- this filter will only be applied on an incremental run
-  where date_rep > (select max(report_date) from {{ this }})
+  where cases.date_rep > (select max(report_date) from {{ this }})
 
 {% endif %}
