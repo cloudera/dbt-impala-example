e455f14f019a289561551931862149c7f6370caa
sdairs <me@alasdairb.com>
2022-04-07 16:48:35 +0100

    add type casts for columns

--- dbt_impala_demo/models/staging/covid/stg_covid__cases_view.sql
+++ dbt_impala_demo/models/staging/covid/stg_covid__cases_view.sql
@@ -1,6 +1,6 @@
 select 
   cast(cast(unix_timestamp(date_rep, "dd/MM/yyyy") as timestamp) AS DATE) as date_rep, 
-  cases, 
-  deaths, 
+  cast(cases as BIGINT) as cases, 
+  cast(deaths as BIGINT) as deaths, 
   geo_id
 from {{ source('raw_covid','raw_covid__cases') }};
