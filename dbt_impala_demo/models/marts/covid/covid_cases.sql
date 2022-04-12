54ea2b2abf117c6bf0e6b0faf401e0745696d5f9
sdairs <me@alasdairb.com>
2022-04-12 16:33:34 +0100

    Add partitions to covid_cases mart model

--- dbt_impala_demo/models/marts/covid/covid_cases.sql
+++ dbt_impala_demo/models/marts/covid/covid_cases.sql
@@ -1,6 +1,7 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+        partition_by='report_date'
     )
 }}
 
@@ -17,17 +18,25 @@ country_codes as(
     country,
     alpha_2code
   from {{ ref('ref__country_codes') }}
-)
+), collected as(
 select 
     cases.date_rep as report_date, 
     cases.cases as cases, 
     cases.deaths as deaths,
     country_codes.country as country
     from cases join country_codes on cases.geo_id = country_codes.alpha_2code
+)
+select 
+  collected.cases,
+  collected.deaths,
+  collected.country,
+  collected.report_date 
+from collected
+
 
 {% if is_incremental() %}
 
   -- this filter will only be applied on an incremental run
-  where cases.date_rep > (select max(report_date) from {{ this }})
+  where collected.report_date > (select max(report_date) from {{ this }})
 
 {% endif %}
